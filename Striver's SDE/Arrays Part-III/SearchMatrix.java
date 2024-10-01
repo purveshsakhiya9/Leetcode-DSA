53b9c011de61007f06bd5f1328a84bf323df2e86
purveshsakhiya9 <purveshsakhiya99@gmail.com>
2024-10-01 10:50:58 +0530

    Search element in the matrix with optimal approach

--- Striver's SDE/Arrays Part-III/SearchMatrix.java
+++ Striver's SDE/Arrays Part-III/SearchMatrix.java
@@ -1,4 +1,5 @@
 public class SearchMatrix {
+//    Search in a matrix with complexity O(M+N)
     public static boolean SearchMatrix(int[][] matrix, int target){
         int i = matrix.length-1;
         int j = matrix[0].length-1;
@@ -6,7 +7,6 @@ public class SearchMatrix {
             return false;
         }
         while(i>=0 && j>=0){
-            System.out.println(matrix[i][j]);
             if(matrix[i][j]>=target && matrix[i][0]<=target){
                 if(matrix[i][j]==target){
                     return true;
@@ -19,8 +19,29 @@ public class SearchMatrix {
         }
         return false;
     }
+
+//    Search in a matrix with complexity O(log(M*N))
+    public static boolean optimalSearchMatrix(int[][] matrix,int target){
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int left = 0, right = (row*col)-1;
+        while(left<=right){
+            int mid = (right+left)/2;
+            int rowidx = mid/col;
+            int colidx = mid%col;
+            if(matrix[rowidx][colidx]==target){
+                return true;
+            }else if(matrix[rowidx][colidx]<target){
+                left = mid+1;
+            }else{
+                right = mid-1;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args){
         int[][] matrix = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
-        System.out.println(SearchMatrix(matrix,16));
+        System.out.println(SearchMatrix(matrix,1));
+        System.out.println(optimalSearchMatrix(matrix,10));
     }
 }
