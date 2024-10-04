fd85c7fd6bf7d80bdfc5a4dc79403d13efc55765
purveshsakhiya9 <purveshsakhiya99@gmail.com>
2024-10-04 19:11:19 +0530

    Added Optimal approach

--- Striver's SDE/Arrays Part-II/InversionOfArray.java
+++ Striver's SDE/Arrays Part-II/InversionOfArray.java
@@ -1,5 +1,5 @@
 public class InversionOfArray {
-    public static int BruteForceApproac(int[] nums){
+    public static int BruteForceApproach(int[] nums){
         int count = 0;
         for(int i = 0; i< nums.length; i++){
             for(int j = i; j< nums.length; j++){
@@ -10,8 +10,68 @@ public class InversionOfArray {
         }
         return count;
     }
+
+    private static int OptimalAproach(int[] nums) {
+        return InversionSortandCount(nums,0,nums.length-1);
+    }
+
+    private static int InversionSortandCount(int[] array, int left, int right) {
+        int count = 0;
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            count += InversionSortandCount(array, left, mid);
+            count += InversionSortandCount(array, mid + 1, right);
+            count += Inversionmergeandcount(array, left, mid, right);
+        }
+        return count;
+    }
+
+    private static int Inversionmergeandcount(int[] array, int left, int mid, int right) {
+        int[] leftarray = new int[mid - left + 1];
+        int[] rightarray = new int[right - mid];
+
+        for (int i = 0; i < leftarray.length; i++) {
+            leftarray[i] = array[left + i];
+        }
+        for (int j = 0; j < rightarray.length; j++) {
+            rightarray[j] = array[mid + 1 + j];
+        }
+
+        int i = 0, j = 0, k = left;
+        int count = 0;
+
+        while (i < leftarray.length && j < rightarray.length) {
+            if (leftarray[i] <= rightarray[j]) {
+                array[k] = leftarray[i];
+                i++;
+            } else {
+                count += (leftarray.length - i);
+                array[k] = rightarray[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements
+        while (i < leftarray.length) {
+            array[k] = leftarray[i];
+            i++;
+            k++;
+        }
+        while (j < rightarray.length) {
+            array[k] = rightarray[j];
+            j++;
+            k++;
+        }
+
+        return count;
+    }
+
     public static void main(String[] args){
         int[] nums = {2,5,1,3,4};
-        System.out.println(BruteForceApproac(nums));
+        System.out.println(BruteForceApproach(nums));
+        System.out.println(OptimalAproach(nums));
     }
+
+
 }
