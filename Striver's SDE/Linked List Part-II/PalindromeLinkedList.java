b0ea095e88202c61a01f03efe0dae1a1b55a5d7a
purveshsakhiya9 <purveshsakhiya99@gmail.com>
2024-10-11 19:30:37 +0530

    Added another appraoch in palindrome linked list

--- Striver's SDE/Linked List Part-II/PalindromeLinkedList.java
+++ Striver's SDE/Linked List Part-II/PalindromeLinkedList.java
@@ -28,12 +28,45 @@ public class PalindromeLinkedList {
         curr = curr.next;
         return true;
     }
+
+    public static boolean ReversePalindromeLinkedList(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null && fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode start = ReverseLinkedList(slow.next);
+        while(start!=null){
+            if(start.val!=head.val){
+                return false;
+            }
+            head = head.next;
+            start = start.next;
+        }
+        return true;
+
+    }
+
+    public static ListNode ReverseLinkedList(ListNode head){
+        ListNode curr = head;
+        ListNode prev = null;
+        while(curr!=null){
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+
     public static void main(String[] args){
         // First linked list: 1 -> 2 -> 2 -> 1
         ListNode head = new ListNode(1);
         head.next = new ListNode(2);
         head.next.next = new ListNode(2);
-        head.next.next.next= new ListNode(2);
+        head.next.next.next= new ListNode(1);
         System.out.println(RecursionPalindromeLinkedList(head));
+        System.out.println(ReversePalindromeLinkedList(head));
     }
 }
