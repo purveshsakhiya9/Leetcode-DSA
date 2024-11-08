07ff4ac007fd55da356882a0c5ac01ef52a70b9f
purveshsakhiya9 <purveshsakhiya99@gmail.com>
2024-11-08 21:57:24 +0530

    String To Integer Atoi

---
+++ Striver's SDE/String/StringToIntegerAtoi.java
@@ -0,0 +1,33 @@
+class StringToIntegerAtoi {
+    public static int StringToIntegerAtoi(String s) {
+        s = s.trim();
+        if (s.isEmpty()) {
+            return 0;
+        }
+
+        int ans = 0, i = 0;
+        boolean neg = s.charAt(0) == '-';
+        boolean pos = s.charAt(0) == '+';
+
+        if (neg || pos) {
+            i++;
+        }
+
+        while (i < s.length() && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+
+            if (ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10 && digit > Integer.MAX_VALUE % 10)) {
+                return neg ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+
+            ans = ans * 10 + digit;
+            i++;
+        }
+
+        return neg ? -ans : ans;
+    }
+    public static void mani(String[] args){
+        String s = " -042";
+        System.out.println(StringToIntegerAtoi(s));
+    }
+}
